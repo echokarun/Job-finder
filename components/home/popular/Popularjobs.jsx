1e7562a1bf43628df8288ec0970787ba7a33e566
echokarun <rayamajhikarun@gmail.com>
2024-10-08 14:38:52 +0545

    Implement UI/UX of the Home Screen

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from "react-native";
 import styles from "./popularjobs.style";
 import { useRouter } from "expo-router";
 import { COLORS, SIZES } from "../../../constants"; 
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 
 const Popularjobs = () => {
 
   const router = useRouter();
-  const isLoading = false
-  const error = false 
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Developer",
+    num_pages: 1
+  })
+
+  // console.log(data);
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+  };
+  
+
 
   
   return (
@@ -28,7 +39,7 @@ const Popularjobs = () => {
             <Text>Something Went Wrong </Text>
           ) : (
               <FlatList
-                data={[1, 2, 3, 4]}
+                data={data}
                 renderItem={({item}) => (
                   <PopularJobCard
                     item={item}
